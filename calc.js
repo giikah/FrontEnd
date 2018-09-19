6ac400ff8d8920521cef32aeee527b547b06036f
Giikah <jpocovi@gmail.com>
2018-09-19 23:08:09 +0200

    Console Message with applied Rental Advice to input

--- calc.js
+++ calc.js
@@ -1,7 +1,10 @@
 function Calculate(){
 var earn = document.getElementById("earn").value;
+var cost = document.getElementById("cost").value;
+
+var perfectRent = (33 / 100) * earn;
 
     document.getElementById("test").innerHTML = earn;
 
-console.log(earn);
+console.log("You should pay no more than:" + " " + perfectRent + " for your rent.");
 }
